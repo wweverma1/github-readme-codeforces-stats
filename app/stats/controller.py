c7076e62dca47ea550ec74c70524f2d145a7353e
wweverma1 <wweverma1@gmail.com>
2022-05-11 18:04:40 +0530

    visualizing graph in dev

--- app/stats/controller.py
+++ app/stats/controller.py
@@ -8,6 +8,10 @@ from app.stats.models import (
     User,
 )
 
+# for creating a pie chart
+# import matplotlib.pyplot as plt
+# import numpy as np
+
 
 def fetch_stats():
     username = request.args.get('u', type=str)
@@ -15,6 +19,11 @@ def fetch_stats():
     user_details = User.fetch_user_details(username)
     if user_details["status"]=="OK":
         submission_details = User.fetch_submission_details(username)
+        # creating a pie chart
+            # y = np.array([submission_details["ac"], submission_details["tle"], submission_details["wa"], submission_details["others"]])
+            # mylabels = ["Accepted", "Time Limited Exceeeded", "Wrong Answer", "Others"]
+            # plt.pie(y, labels = mylabels)
+            # plt.show()
         return jsonify({"status": "OK", "userDetails": user_details["userDetails"], "submissionDetails": submission_details}), 200
     else:
         comment=user_details["comment"]
