0e52e255693554be6b727e5d039fb43b48ed70bc
wweverma1 <wweverma1@gmail.com>
2022-05-11 18:03:44 +0530

    parsing user submissions and sorting them

--- app/stats/controller.py
+++ app/stats/controller.py
@@ -1,35 +1,22 @@
 # Related third party imports
-import json
-import requests
 from flask import (
     jsonify,
     request,
+)
+
+from app.stats.models import (
+    User,
 )
 
 
 def fetch_stats():
     username = request.args.get('u', type=str)
     theme_id = request.args.get('t', type=int, default=1)
-    payload = {'handles': username}
-    cfResponse = requests.get('https://codeforces.com/api/user.info', params=payload)
-    if cfResponse.status_code==200:
-        status="OK"
-        cfResponse=cfResponse.json()
-        result = {
-            "handle": cfResponse["result"][0]["handle"],
-            "titlePhoto": cfResponse["result"][0]["titlePhoto"],
-            "organization": cfResponse["result"][0]["organization"],
-            "rank": cfResponse["result"][0]["rank"],
-            "rating": cfResponse["result"][0]["rating"],
-            "maxRank": cfResponse["result"][0]["maxRank"],
-            "maxRating": cfResponse["result"][0]["maxRating"],
-            "cardTheme": "light" if theme_id==1 else "dark",
-        }
-        return jsonify({"status": status, "result": result}), 200
+    user_details = User.fetch_user_details(username)
+    if user_details["status"]=="OK":
+        submission_details = User.fetch_submission_details(username)
+        return jsonify({"status": "OK", "userDetails": user_details["userDetails"], "submissionDetails": submission_details}), 200
     else:
-        status="FAILED"
-        if cfResponse.status_code==400:
-            comment=cfResponse.json()["comment"]
-        else:
-            comment="Internal Server Error"
-    return jsonify({"status": status, "comment": comment}), 400
+        comment=user_details["comment"]
+    return jsonify({"status": "FAILED", "comment": comment}), 400
+    
